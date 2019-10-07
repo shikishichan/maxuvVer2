ea516deadc4efe9e7480622b43edd130decdf8da
Kisho Shiraishi <e175761@ie.u-ryukyu.ac.jp>
2019-10-07 18:56:03 +0900

    Initial Commit

---
+++ BookLogVer2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BookLogVer2
+//
+//  Created by kisho shiraishi on 2019/10/07.
+//  Copyright © 2019 kisho shiraishi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
