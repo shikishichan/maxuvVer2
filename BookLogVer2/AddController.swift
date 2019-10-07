f7caaf2f9504cb9bbc632d4131b176a196e2a398
Kisho Shiraishi <e175761@ie.u-ryukyu.ac.jp>
2019-10-07 20:32:09 +0900

    本登録と一覧の実装

---
+++ BookLogVer2/AddController.swift
@@ -0,0 +1,37 @@
+//
+//  AddController.swift
+//  BookLogVer2
+//
+//  Created by kisho shiraishi on 2019/10/07.
+//  Copyright © 2019 kisho shiraishi. All rights reserved.
+//
+
+import UIKit
+var TodoKobetsunonakami = [String]()
+
+class AddController: UIViewController {
+    @IBOutlet weak var TodoTextField: UITextField!
+    @IBAction func TodoAddButton(_ sender: Any) {
+        TodoKobetsunonakami.append(TodoTextField.text!)
+        TodoTextField.text = ""
+        UserDefaults.standard.set( TodoKobetsunonakami, forKey: "TodoList" )
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BookLogVer2/FirstViewController.swift
+++ BookLogVer2/FirstViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TodoKobetsunonakami.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let TodoCell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        TodoCell.textLabel!.text = TodoKobetsunonakami[indexPath.row]
+        return TodoCell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if UserDefaults.standard.object(forKey: "TodoList") != nil {
+            TodoKobetsunonakami = UserDefaults.standard.object(forKey: "TodoList") as! [String]
+        }
         // Do any additional setup after loading the view.
         
         //hoge
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 }
 
