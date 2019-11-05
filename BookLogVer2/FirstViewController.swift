8519efc4b8566dc95e83dd29f7f511303f200094
kazuma uehara <e175740@ie.u-ryukyu.ac.jp>
2019-11-05 13:03:08 +0900

    tableviewの編集モード(削除と並べ替え)を実装した

--- BookLogVer2/FirstViewController.swift
+++ BookLogVer2/FirstViewController.swift
@@ -21,7 +21,6 @@ class FirstViewController:  UIViewController, UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
         
         if UserDefaults.standard.object(forKey: "SectionList") != nil{
             mySections = UserDefaults.standard.object(forKey: "SectionList") as! [String]
@@ -37,7 +36,7 @@ class FirstViewController:  UIViewController, UITableViewDataSource, UITableView
             }
         }
         
-       // editボタンの追加だけ、動作はまだ
+        
         self.navigationController?.isNavigationBarHidden = false
         navigationItem.title = "一覧画面"
         navigationItem.rightBarButtonItem = editButtonItem
@@ -66,6 +65,53 @@ class FirstViewController:  UIViewController, UITableViewDataSource, UITableView
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedClass = mySections[indexPath.section]
         selectedBook = twoDimArray[indexPath.section][indexPath.row]
+        
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        //override前の処理を継続してさせる
+        super.setEditing(editing, animated: animated)
+        //tableViewの編集モードを切り替える
+        tableView.isEditing = editing //editingはBool型でeditButtonに依存する変数
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        //dataを消してから
+        twoDimArray[indexPath.section].remove(at: indexPath.row)
+        UserDefaults.standard.set(twoDimArray[indexPath.section], forKey: mySections[indexPath.section])
+
+        //tableViewCellの削除
+        self.tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
+    //通常時(editモードでない)にセルを左にスワイプするとdeleteが出てくるのをなくす
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if tableView.isEditing {
+            return .delete
+        }
+        return .none
+    }
+    
+    //並び替えボタン
+    func tableView(
+        _ tableView: UITableView,
+        moveRowAt sourceIndexPath: IndexPath,
+        to destinationIndexPath: IndexPath) {
+        
+        //同一section内ならswap, 別のsectionならinsert&remove
+        if(sourceIndexPath.section == destinationIndexPath.section){
+            twoDimArray[sourceIndexPath.section].swapAt(sourceIndexPath.row, destinationIndexPath.row)
+        }else{
+            twoDimArray[destinationIndexPath.section].insert(twoDimArray[sourceIndexPath.section][sourceIndexPath.row], at: destinationIndexPath.row)
+            twoDimArray[sourceIndexPath.section].remove(at: sourceIndexPath.row)
+        }
+        
+        //変更を保存
+        var count = 0
+        for i in mySections{
+            UserDefaults.standard.set(twoDimArray[count], forKey: i)
+            count += 1
+        }
     }
     
     override func didReceiveMemoryWarning() {
