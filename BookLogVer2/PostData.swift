e3a86730fcdd30c2bb22355ce228b856049e8247
hoge <e175728@ie.u-ryukyu.ac.jp>
2019-11-07 00:46:41 +0900

    本棚をPOSTする関数を追加

--- BookLogVer2/PostData.swift
+++ BookLogVer2/PostData.swift
@@ -55,9 +55,51 @@ class PostData{
         })
         task.resume()
     }
-
     
-
     
+    func post_shelf(data:BookShelf){
+
+        let url = URL(string: "https://booklog-api.herokuapp.com/api/bookshelfs")!
+
+        let session = URLSession.shared
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+
+        do {
+            request.httpBody = try JSONEncoder().encode(data)
+            
+        } catch let error {
+
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+
+            guard error == nil else {
+                return
+            }
+
+            guard let data = data else {
+                return
+            }
+
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
+    }
 
 }
