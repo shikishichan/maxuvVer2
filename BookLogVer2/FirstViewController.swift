913eed946dbf60bf7fb2639169ae1ea3c2b1a283
hoge <e175728@ie.u-ryukyu.ac.jp>
2019-11-16 16:35:58 +0900

    DBからデータを取得して表示するまでを実装

--- BookLogVer2/FirstViewController.swift
+++ BookLogVer2/FirstViewController.swift
@@ -13,6 +13,10 @@ class FirstViewController:  UIViewController, UITableViewDataSource, UITableView
     var twoDimArray = [[String]]()
     var selectedClass = ""
     var selectedBook = ""
+    var booklist:[Book] = []
+    var view_list:[[Book]] = []
+    var book_shelf_list:[BookShelf] = []
+    let get_api = GetDataFromApi()
     
 
     @IBOutlet weak var tableView: UITableView!
@@ -25,58 +29,112 @@ class FirstViewController:  UIViewController, UITableViewDataSource, UITableView
         self.navigationController?.isNavigationBarHidden = false
         navigationItem.title = "一覧画面"
         navigationItem.rightBarButtonItem = editButtonItem
+        
+        //カスタムセルの設定
+        tableView.register (UINib(nibName: "TableViewCell", bundle: nil),forCellReuseIdentifier:"reuse_cell")
+        
+        //データベースからデータのロード
+        load_data()
     }
         
       
     //表示時のデータ更新
     override func viewWillAppear(_ animated: Bool) {
 
-        
-        if UserDefaults.standard.object(forKey: "SectionList") != nil{
-            mySections = UserDefaults.standard.object(forKey: "SectionList") as! [String]
-        }
-        twoDimArray = []
-        for i in mySections{
-            if UserDefaults.standard.object(forKey: i) != nil {
-                let x = UserDefaults.standard.object(forKey: i) as! [String]
-                twoDimArray.append(x)
-                
-            }else{
-                UserDefaults.standard.set([], forKey: i)
-                twoDimArray.append([])
-            }
-        }
-        
+
+//        if UserDefaults.standard.object(forKey: "SectionList") != nil{
+//            mySections = UserDefaults.standard.object(forKey: "SectionList") as! [String]
+//        }
+//        twoDimArray = []
+//        for i in mySections{
+//            if UserDefaults.standard.object(forKey: i) != nil {
+//                let x = UserDefaults.standard.object(forKey: i) as! [String]
+//                twoDimArray.append(x)
+//
+//            }else{
+//                UserDefaults.standard.set([], forKey: i)
+//                twoDimArray.append([])
+//            }
+//        }
+
         tableView.reloadData()
-        super.viewWillAppear(animated)
+//        super.viewWillAppear(animated)
+
+    }
     
+    func load_data(){
+    let dispatchGroup = DispatchGroup()
+    let shelfQueue = DispatchQueue(label: "queue", qos: .userInteractive)
+    let bookQueue = DispatchQueue(label: "queue", qos: .userInteractive)
+    
+    //本と本棚のapiを取得するため、未完了タスクを２つ用意する
+    dispatchGroup.enter()
+    dispatchGroup.enter()
+    
+    shelfQueue.async(group: dispatchGroup) {
+        self.get_api.get_book_shelf(completion: {returnData in
+            self.book_shelf_list = returnData
+            
+            for _ in self.book_shelf_list{
+                self.view_list.append([])
+            }
+            dispatchGroup.leave()
+            print("shelfDone")
+        })
+    }
+    
+    
+    bookQueue.async(group: dispatchGroup){
+        self.get_api.get_book(completion: {returnData in
+                self.booklist = returnData
+                dispatchGroup.leave()
+                print("bookDone")
+        })
+    }
+    
+    //本と本棚両方のapiを取得後リロード
+    dispatchGroup.notify(queue: .main) {
+        var count:Int = 0
+        
+        for i in self.book_shelf_list{
+            for j in self.booklist{
+                if i.id == j.place_id{
+                    self.view_list[count].append(j)
+                }
+            }
+            count += 1
+        }
+        print("All Process Done!")
+        self.tableView.reloadData()
+        
+    }
     }
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return mySections.count
+        return book_shelf_list.count
 
      }
      
       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return twoDimArray[section].count
+            return view_list[section].count
         }
      
       func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-         return mySections[section]
+        return book_shelf_list[section].name
      }
     
      
       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
          let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
-         cell.textLabel?.text = twoDimArray[indexPath.section][indexPath.row]
+        cell.textLabel?.text = view_list[indexPath.section][indexPath.row].title
          cell.textLabel!.font = UIFont(name: "Arial", size: 20)//cellのfont,size
          return cell
      }
      
      func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-         selectedClass = mySections[indexPath.section]
-         selectedBook = twoDimArray[indexPath.section][indexPath.row]
+        selectedClass = book_shelf_list[indexPath.section].name
+        selectedBook = view_list[indexPath.section][indexPath.row].title
      }
     
     override func setEditing(_ editing: Bool, animated: Bool) {
@@ -88,8 +146,9 @@ class FirstViewController:  UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         //dataを消してから
-        twoDimArray[indexPath.section].remove(at: indexPath.row)
-        UserDefaults.standard.set(twoDimArray[indexPath.section], forKey: mySections[indexPath.section])
+        view_list[indexPath.section].remove(at: indexPath.row)
+        //データベースにpostする
+        //UserDefaults.standard.set(twoDimArray[indexPath.section], forKey: book_shelf_list[indexPath.section])
 
         //tableViewCellの削除
         self.tableView.deleteRows(at: [indexPath], with: .automatic)
@@ -111,18 +170,18 @@ class FirstViewController:  UIViewController, UITableViewDataSource, UITableView
         
         //同一section内ならswap, 別のsectionならinsert&remove
         if(sourceIndexPath.section == destinationIndexPath.section){
-            twoDimArray[sourceIndexPath.section].swapAt(sourceIndexPath.row, destinationIndexPath.row)
+            view_list[sourceIndexPath.section].swapAt(sourceIndexPath.row, destinationIndexPath.row)
         }else{
-            twoDimArray[destinationIndexPath.section].insert(twoDimArray[sourceIndexPath.section][sourceIndexPath.row], at: destinationIndexPath.row)
-                twoDimArray[sourceIndexPath.section].remove(at: sourceIndexPath.row)
+            view_list[destinationIndexPath.section].insert(view_list[sourceIndexPath.section][sourceIndexPath.row], at: destinationIndexPath.row)
+                view_list[sourceIndexPath.section].remove(at: sourceIndexPath.row)
         }
         
         //変更を保存
-        var count = 0
-        for i in mySections{
-            UserDefaults.standard.set(twoDimArray[count], forKey: i)
-            count += 1
-        }
+//        var count = 0
+//        for i in book_shelf_list{
+//            UserDefaults.standard.set(twoDimArray[count], forKey: i)
+//            count += 1
+//        }
     }
     
     
@@ -135,7 +194,7 @@ class FirstViewController:  UIViewController, UITableViewDataSource, UITableView
         label.backgroundColor = UIColor.init(red: 205/255, green: 133/255, blue: 63/255, alpha: 100/100)
         label.textColor = UIColor.white
         label.font = UIFont.systemFont(ofSize: 20)
-        label.text = "  "+mySections[section]
+        label.text = "  "+book_shelf_list[section].name
         return label
     }//sectionの色、文字サイズ
     
